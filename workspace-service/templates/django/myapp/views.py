09e4304f189039e2a80a6f2cf92566ffa09713ac
rounak bhatia <rounak@contlo.com>
2024-07-04 18:44:10 +0530

    added template and prompt

---
+++ workspace-service/templates/django/myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')

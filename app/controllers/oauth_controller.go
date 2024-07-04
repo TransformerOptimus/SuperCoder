a42c833bbab8060319d9bd1f9c3010764c9d0c08
abhijeet <abhijeet@contlo.com>
2024-07-04 15:44:39 +0530

    Add check for development

--- app/controllers/oauth_controller.go
+++ app/controllers/oauth_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"ai-developer/app/config"
+	"ai-developer/app/constants"
 	"ai-developer/app/services"
 	"fmt"
 	"github.com/gin-gonic/gin"
@@ -21,6 +22,18 @@ type OauthController struct {
 }
 
 func (controller *OauthController) GithubSignIn(c *gin.Context) {
+	var env = config.Get("app.env")
+	fmt.Println("ENV : ", env)
+	if env == constants.Development {
+		fmt.Println("Handling Skip Authentication Token.....")
+		redirectURL, err := controller.authService.HandleDefaultAuth()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to get default user token"})
+			return
+		}
+		c.Redirect(http.StatusTemporaryRedirect, redirectURL)
+
+	}
 	var githubOauthConfig = &oauth2.Config{
 		RedirectURL:  controller.redirectURL,
 		ClientID:     controller.clientID,

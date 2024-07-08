b3479ada32a656b8a1e4d32a25c6aa6826729816
parthpathak <parth.pathak@science.christuniversity.in>
2024-07-08 19:32:38 +0530

    pushing preload fixes

--- gui/src/app/(programmer)/layout.tsx
+++ gui/src/app/(programmer)/layout.tsx
@@ -1,48 +1,36 @@
 'use client';
-
 import '../_app.css';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import SideBar from '@/components/LayoutComponents/SideBar';
 import NavBar from '@/components/LayoutComponents/NavBar';
-import { usePathname } from 'next/navigation';
 import Code from '@/app/(programmer)/code/Code';
 import { Toaster } from 'react-hot-toast';
 import { SocketProvider } from '@/context/SocketContext';
 
 export default function ProgrammerLayout({
-  children,
-}: {
+                                           children,
+                                         }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const [isCodeRendered, setIsCodeRendered] = useState(false);
-
-  useEffect(() => {
-    if (pathname === '/code') {
-      setIsCodeRendered(true);
-    }
-  }, [pathname]);
-
   useEffect(() => {
     if (typeof window !== 'undefined' && window.clarity) {
-      console.log('Email Tracked');
       window.clarity('set', 'User Email', localStorage.getItem('userEmail'));
     }
   }, []);
 
   return (
-    <div className="layout">
-      <NavBar />
-      <div className="main_content">
-        <SideBar />
-        <SocketProvider>
-          <section className="content">
-            {isCodeRendered && <Code />}
-            {children}
-          </section>
-        </SocketProvider>
-        <Toaster />
+      <div className="layout">
+        <NavBar />
+        <div className="main_content">
+          <SideBar />
+          <SocketProvider>
+            <section className="content">
+              <Code />
+              {children}
+            </section>
+          </SocketProvider>
+          <Toaster />
+        </div>
       </div>
-    </div>
   );
 }

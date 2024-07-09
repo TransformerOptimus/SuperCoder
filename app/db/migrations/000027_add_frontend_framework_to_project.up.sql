2984ee1be8407af38a33521aab25af1e8778eaa1
rounak bhatia <rounak@contlo.com>
2024-07-09 14:33:38 +0530

    changes in workspace service and migration file

---
+++ app/db/migrations/000027_add_frontend_framework_to_project.up.sql
@@ -0,0 +1,9 @@
+ALTER TABLE projects
+ADD COLUMN frontend_framework VARCHAR(100);
+
+-- Update existing rows with an empty string
+UPDATE projects SET frontend_framework = '' WHERE frontend_framework IS NULL;
+
+-- Alter the column to NOT NULL
+ALTER TABLE projects
+ALTER COLUMN frontend_framework SET NOT NULL;

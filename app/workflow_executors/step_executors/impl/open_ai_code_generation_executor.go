0d3f4a6144d3ba8cb7a5f3d9d5076645309ac39d
rounak bhatia <rounak@contlo.com>
2024-07-10 19:27:47 +0530

    merge onflicts resolved

--- app/workflow_executors/step_executors/impl/open_ai_code_generation_executor.go
+++ app/workflow_executors/step_executors/impl/open_ai_code_generation_executor.go
@@ -204,6 +204,7 @@ func (openAICodeGenerator OpenAICodeGenerator) Execute(step steps.GenerateCodeSt
 		return errors.New("LLM API Key not found in database")
 	}
 	apiKey := llmAPIKey.LLMAPIKey
+	fmt.Println("_________API_KEY_________", apiKey)
 	framework := project.Framework
 	fmt.Println("_________FRAMEWORK_________", framework)
 	// Generate code using the final instruction

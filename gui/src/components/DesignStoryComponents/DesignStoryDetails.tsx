572df4ed4bccb1770b22e948b3f7718f8f399bdd
shaarang-contlo <shaarang.prabhudesai@contlo.com>
2024-07-18 16:43:58 +0530

    in review workflow for design stories

--- gui/src/components/DesignStoryComponents/DesignStoryDetails.tsx
+++ gui/src/components/DesignStoryComponents/DesignStoryDetails.tsx
@@ -3,14 +3,20 @@ import { DesignStoryDetailsProps } from '../../../types/designStoryTypes';
 import imagePath from '@/app/imagePath';
 import CustomImage from '@/components/ImageComponents/CustomImage';
 import CustomDropdown from '@/components/CustomDropdown/CustomDropdown';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CustomTag from '@/components/CustomTag/CustomTag';
 import { handleInProgressStoryStatus, handleStoryStatus } from '@/app/utils';
 import { useDesignContext } from '@/context/Design';
 import { deleteStory, updateStoryStatus } from '@/api/DashboardService';
-import { storyStatus } from '@/app/constants/BoardConstants';
+import { storyStatus, storyActions } from '@/app/constants/BoardConstants';
 import { useRouter } from 'next/navigation';
 import { Button } from '@nextui-org/react';
+
+interface Issue {
+  title: string | null;
+  description: string | null;
+  actions: { label: string; link: string }[];
+}
 
 const DesignStoryDetails: React.FC<DesignStoryDetailsProps> = ({
   id,
@@ -22,6 +28,61 @@ const DesignStoryDetails: React.FC<DesignStoryDetailsProps> = ({
 }) => {
   const { selectedStory, setEditTrue } = useDesignContext();
   const router = useRouter();
+
+  const [issue, setIssue] = useState<Issue | null>({
+    title: null,
+    description: null,
+    actions: [],
+  });
+
+  const resetIssue = () => {
+    setIssue({
+      title: null,
+      description: null,
+      actions: [],
+    });
+  };
+
+  useEffect(() => {
+    if (selectedStory && selectedStory.status === storyStatus.IN_REVIEW) {
+      let issueTitle = '';
+      let issueDescription = '';
+      const actions = [];
+
+      switch (selectedStory.reason) {
+        case storyStatus.MAX_LOOP_ITERATIONS:
+          issueTitle = 'Action Needed: Maximum number of iterations reached';
+          issueDescription =
+            'The story execution in the workbench has exceeded the maximum allowed iterations. You can update the story details and re-build it.';
+          actions.push(
+            { label: 'Re-Build', link: storyActions.REBUILD },
+            {
+              label: 'Get Help',
+              link: 'https://discord.com/invite/dXbRe5BHJC',
+            },
+          );
+          break;
+        case storyStatus.LLM_KEY_NOT_FOUND:
+          issueTitle = 'Action Needed: LLM API Key Configuration Error';
+          issueDescription =
+            'There is an issue with the LLM API Key configuration, which may involve an invalid or expired API key. Please verify the API Key settings and update them to continue.';
+          actions.push(
+            { label: 'Re-Build', link: storyActions.REBUILD },
+            { label: 'Go to Settings', link: '/settings' },
+          );
+          break;
+      }
+
+      setIssue({
+        title: issueTitle,
+        description: issueDescription,
+        actions,
+      });
+    } else {
+      resetIssue();
+    }
+  }, [selectedStory]);
+
   const handleEditAction = () => {
     setEditTrue(true);
     close();
@@ -171,6 +232,59 @@ const DesignStoryDetails: React.FC<DesignStoryDetailsProps> = ({
             />
           </div>
         </div>
+        {selectedStory.status === storyStatus.IN_REVIEW && (
+          <div
+            className={`${styles.issue_container} m-4 flex flex-row gap-2 rounded-lg p-3`}
+          >
+            <CustomImage
+              className={'mt-[2px] size-5'}
+              src={imagePath.overviewWarningYellow}
+              alt={'error_icon'}
+            />
+
+            <div
+              id={'in_review_description'}
+              className={'proxima_nova flex flex-col gap-2 text-white'}
+            >
+              <span id={'issue_title'} className={'text-base font-medium'}>
+                {issue?.title}
+              </span>
+
+              <span
+                id={'issue_description'}
+                className={'text-sm font-normal opacity-80'}
+              >
+                {issue?.description}
+              </span>
+
+              <div
+                id={'issue_action_buttons'}
+                className={'my-2 flex flex-row gap-2'}
+              >
+                {issue?.actions?.length > 0 &&
+                  issue.actions.map((action, index) => (
+                    <Button
+                      key={index}
+                      onClick={() => {
+                        if (action.link === storyActions.REBUILD) {
+                          handleMoveToInProgressClick().then().catch();
+                        } else {
+                          router.push(action.link);
+                        }
+                      }}
+                      className={
+                        action.label === storyActions.REBUILD
+                          ? 'primary_medium'
+                          : 'light_medium'
+                      }
+                    >
+                      {action.label}
+                    </Button>
+                  ))}
+              </div>
+            </div>
+          </div>
+        )}
         <div className={'flex flex-col gap-8 p-4'}>
           <div className={'flex flex-col gap-2'}>
             <span className={`secondary_color text-sm font-normal`}>

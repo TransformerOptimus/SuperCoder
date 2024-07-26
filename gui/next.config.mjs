23e402f2bc48ddfc72b02b647d0963d9bac4d20a
rounak bhatia <rounak@contlo.com>
2024-07-26 18:27:33 +0530

    frontend fixes

--- gui/next.config.mjs
+++ gui/next.config.mjs
@@ -1,10 +1,8 @@
 const localurl = process.env.NEXT_PUBLIC_DOCKER
   ? 'http://server:8080/api/:path*'
   : 'http://0.0.0.0:8080/api/:path*';
-const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
-  ? `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`
-  : 'http://default-backend-url/api/:path*';
 
+let backendUrl = process.env.NEXT_PUBLIC_API_URL+'/api/:path*';
 
 console.log("NEXT_PUBLIC_DOCKER", process.env.NEXT_PUBLIC_DOCKER)
 console.log("LOCALURL", localurl)
@@ -16,7 +14,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: localurl,
+        destination: process.env.NEXT_PUBLIC_APP_ENV === 'development' ? localurl : backendUrl,
       },
     ];
   },
